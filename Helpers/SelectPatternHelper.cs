7c61bb2b3ecd0d152a59ce36b807c73bd6d43eb5
Camilo Certuche <camilo.certuche@gmail.com>
2022-09-14 14:56:48 -0500

    Simple menu to choose the pattern example to be executed

--- Helpers/SelectPatternHelper.cs
+++ Helpers/SelectPatternHelper.cs
@@ -1,10 +1,37 @@
 using Interfaces;
+using Examples.Observer;
 using Examples.Strategy;
 
 namespace Helpers {
   public static class SelectPatternHelper {
     public static IDesignPatternExample Execute() {
-      return new StrategyPattern();
+      return BuildPatternExample(ReadOption());
+    }
+    private static int ReadOption() {
+      int option;
+
+      do {
+        ShowOptions();
+        int.TryParse(Console.ReadLine(), out option);
+      } while(!optionIsValid(option));
+
+      return option;
+    }
+
+    private static void ShowOptions() {
+      Console.WriteLine("Select a design pattern example\n");
+      Console.WriteLine("0. Strategy");
+      Console.WriteLine("1. Observer");
+    }
+    private static bool optionIsValid(int option) => 0 <= option && option < 2;
+
+    private static IDesignPatternExample BuildPatternExample(int option) {
+      switch (option) {
+        case 1:
+          return new ObserverPattern();
+        default:
+          return new StrategyPattern();
+      }
     }
   }
 }
